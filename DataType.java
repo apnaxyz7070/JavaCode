7974ef3e66a54a1013d3ef4919a856243de274b4
apnaxyz7070 <130081066+apnaxyz7070@users.noreply.github.com>
2023-04-08 12:23:01 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,45 @@
+class DataType{
+    public static void main(String a[]) {
+        
+        int num1=9;
+        byte by=127;
+        short sh=553;
+        long l=450125l;
+
+        float ft=54.25f;
+        double d=5.5;
+
+        char ch='R';
+
+        boolean b=true;
+
+       System.out.println(num1);
+       System.out.println(by);
+       System.out.println(sh);
+       System.out.println(l);
+       System.out.println(ft);
+       System.out.println(d);
+       System.out.println(ch);
+       System.out.println(b);
+
+   }
+}
+/* 
+
+OUTPUT :-
+
+PS D:\JavaCode> javac .\DataType.java
+PS D:\JavaCode> java DataType        
+9
+127
+553
+450125
+54.25
+5.5
+R
+true
+PS D:\JavaCode> 
+
+
+
+*/
