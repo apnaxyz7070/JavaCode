a6e26e2ead1f5881dac1d272138c6f703c33200f
apnaxyz7070 <130081066+apnaxyz7070@users.noreply.github.com>
2023-04-09 11:50:18 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,60 @@
+
+
+public class AssignmentOperators{
+    
+   public static void main(String[] args) {
+     
+/* 
+    int n1=5;
+    int n2=4;
+    int result=n1+n2;
+    System.out.println(result);
+ */ 
+   
+  /*
+    int n1=5;
+    int n2=4;
+    int result=n1-n2;
+    System.out.println(result);
+*/
+
+/*
+    int n1=5;
+    int n2=4;
+    int result=n1*n2;
+    System.out.println(result);
+*/
+
+/*
+    int n1=5;
+    int n2=4;
+    int result=n1/n2;
+    System.out.println(result);
+*/
+
+/*
+    int n1=5;
+    int n2=4;
+    int result=n1%n2;
+    System.out.println(result);
+*/
+
+     int num=45;
+ //   num=num+4;
+ //   num+=4;
+ //   num*=2;
+
+       num++;       // Post Increment
+       ++num;       // Pre Increment
+       num--;       // Post Decrement
+       --num;       // Pre Derement
+       System.out.println(num);
+
+       int result=num++;   //Fetch the value and then increment.
+
+       System.out.println(result);
+
+
+
+   }
+}
