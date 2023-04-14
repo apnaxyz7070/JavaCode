64ae5638d266b7fd25c0fae08673705b908c7199
apnaxyz7070 <130081066+apnaxyz7070@users.noreply.github.com>
2023-04-15 05:20:05 +0530

    Add files via upload

---
+++ CreationOfArray.java
@@ -0,0 +1,38 @@
+
+public class CreationOfArray{
+	public static void main(String[] args) {
+//      int nums[]= {3,7,2,4};
+//		nums[1]=6;
+//		System.out.println(nums[1]);
+		
+		int nums[]=new int[4];
+		nums[0]=4;
+		nums[1]=8;
+		nums[2]=3;
+		nums[3]=9;
+		
+//		System.out.println(nums[0]);
+//		System.out.println(nums[1]);
+//		System.out.println(nums[2]);
+//		System.out.println(nums[3]);
+		
+		for (int i=0;i<4;i++) {
+			System.out.println(nums[i]);
+		}
+		
+	}
+}
+
+/*
+
+OUTPUT:
+
+PS D:\JavaCode> javac .\CreationOfArray.java
+PS D:\JavaCode> java CreationOfArray        
+4
+8
+3
+9
+PS D:\JavaCode> 
+
+ */
