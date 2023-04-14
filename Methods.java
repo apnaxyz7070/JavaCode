2bf9c936f036e27c6fa5cf623c26b33d47228a3e
apnaxyz7070 <130081066+apnaxyz7070@users.noreply.github.com>
2023-04-15 05:24:48 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,60 @@
+
+/**
+class Calculator{
+	int a;
+	public int add(int n1, int n2)
+	{
+		int r=n1+n2;
+		return r;
+	}
+}
+
+public class Methods {
+
+	public static void main(String[] args) {
+		int num1=4;
+		int num2=5;
+		Calculator calc= new Calculator();
+		int result = calc.add(num1,num2);
+		System.out.println(result);
+	}
+}
+	**/
+
+class Computer
+{
+	public void playMusic()
+	{
+		System.out.println("Music Playing...");
+	}
+	public String getMeAPen(int cost)
+	{
+		if(cost>=10)
+			return "Pen";
+		else
+			return "Nothing";
+	}
+}
+
+
+public class Methods{
+	public static void main(String[] args) {
+		Computer obj=new Computer();
+		obj.playMusic();
+		String src=obj.getMeAPen(10);
+		System.out.println(src);
+	}
+}
+
+
+/*
+
+OUTPUT: 
+
+PS D:\JavaCode> javac .\Methods.java
+PS D:\JavaCode> java Methods
+Music Playing...
+Pen
+PS D:\JavaCode> 
+
+*/
