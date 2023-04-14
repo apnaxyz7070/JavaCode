c186079415d2ad14df6f9c7e3c8fd6c8a71f5222
apnaxyz7070 <130081066+apnaxyz7070@users.noreply.github.com>
2023-04-15 05:16:55 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,27 @@
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i=1;
+		do
+		{
+			System.out.println("Hi"+i);
+			i++;
+		}
+		while(i<=4);
+	}
+}
+
+
+/*
+
+OUTPUT:
+
+
+ PS D:\JavaCode> javac .\DoWhileLoop.java
+PS D:\JavaCode> java DoWhileLoop      
+Hi1
+Hi2
+Hi3
+Hi4
+PS D:\JavaCode>
+ */
