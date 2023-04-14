d9a432c036f1ee8318c39da006fdb1d7ceab18fb
apnaxyz7070 <130081066+apnaxyz7070@users.noreply.github.com>
2023-04-15 05:20:42 +0530

    Add files via upload

---
+++ JaggedArray.java
@@ -0,0 +1,27 @@
+public class JaggedArray{
+	public static void main(String[] args) 
+	{
+		int nums[][]=new int [3][]; //jagged
+		nums[0]=new int [3];
+		nums[1]=new int [4];
+		nums[2]=new int [2];
+		
+//		int nums[][]=new int [3][4];
+//		int nums[][]=new int [3][4][5]; // three dimensional
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			for(int j=0;j<nums[i].length;j++)
+			{
+				nums[i][j]=(int)(Math.random()*10);
+			}
+		}
+
+        for(int m[] :nums ){
+        for(int n : m){
+        System.out.println("  |  "+n);
+        }
+        System.out.println();
+    }
+	}
+}
